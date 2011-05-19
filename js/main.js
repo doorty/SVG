d6bedbff1d75415b096a13fec0316417642e1d2a
Brent Daugherty <brent@doorty.com>
2011-05-19 07:59:09 -0700

    subclass array. next parse svg path

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ window.onload = function() {
   var pathOfPoints = new Path([[5, 10], [10, 20], [5, 15]]);
   var pathOfSubpath = new Path('stringy');
   
+  
   if (pathOfPoints instanceof Path) {
     console.log('pathOfPoints instanceof Path === true');
   }
@@ -9,5 +10,18 @@ window.onload = function() {
   if (pathOfSubpath instanceof Array) {
     console.log('pathOfSubpath instanceof Array === true');
   }
+/*
+  var pathSlice = pathOfPoints.slice(0);
+  if (pathSlice instanceof Path) {
+    console.log('pathSlice instanceof Path === true');
+  } else if (pathSlice instanceof Array) {
+      console.log('pathSlice instanceof Array === true');
+  } else {
+    console.log('pathSlice is not a Path or Array instance');
+  }
+*/
+  
+  var result = pathStringToArrayOfPoints('M 100 100 L 300 100 L 200 300 z');
+  console.log(result.toString());
   
 };
--- js/path.js
+++ js/path.js
@@ -1,62 +1,71 @@
-var Path = function(path) {
-
-  if (isArray(path)) {
-    
-    console.log('array of points');
-    
-  } else if (isString(path)) {
-    
-    console.log('string of path');
-    
+// Q: How best to subclass Array? ...see http://perfectionkills.com/how-ecmascript-5-still-does-not-allow-to-subclass-an-array/
+// A: There is no one right way. This way relies on non-standard __proto__ property
+function Path(arg) {
+  var arr = [];
+  if (arg.isArray) {
+    arr.push.apply(arr, arg);
   } else {
-    
-    console.log('Path: invalid argument');
-    
+    arr.push.apply(arr, pathStringToArrayOfPoints(arg));
   }
-
-};
+  arr.__proto__ = Path.prototype;
+  return arr;
+}
 
 Path.prototype = new Array();
-Path.prototype.pathFromPoints = computePathFromPoints;
-Path.prototype.pathFromPathSubset = computePathFromPathSubset;
+
+Path.prototype.last = function() {
+  return this[this.length -1];
+};
+
 Path.prototype.clone = null;
 Path.prototype.toString = null;
-
-// accepts: array of points (which are arrays themselves of length two)
-// returns: ..
-var computePathFromPoints = function(points) {
-
-};
+Path.prototype.map = map;
+Path.prototype.filter = filter;
+Path.prototype.slice = slice;
 
 // accepts: subset of an SVG path string (see http://www.w3.org/TR/SVG/paths.html), 
 // where only the moveto, closepath and lineto commands are accepted
-// returns: ...
-var computePathFromPathSubset = function(path) {
-
+// returns: array of points (which are arrays themselves of length two)
+var pathStringToArrayOfPoints = function(path) {
+  
+  var arr = [];
+  
+  switch('') {
+    case 'M':  // absolute moveto    
+      // push next [x, y] to array
+      break;
+    case 'L':  // absolute lineto
+      // push next [x, y] to array
+      break;
+    case 'l':  // relative lineto
+      // get last [x0, y0], add [x1, x2], push [x0+x1, y0+y1] to last 
+      break;
+    case 'z':  // closepath. this is optional. should always close path when drawn (do not add to array?)
+      break;
+  }
+  
+  
+  return [[0, 1]];
 };
 
 // returns a new Path with the same points
 var clone = function() {
-  
+  return new Path(this.arrayOfPoints);
 };
 
 // returns an SVG path string representing the Path
 var toString = function() {
   
-}
+};
 
-// map, filter, and slice must return Path objects rather than pure arrays
-var map;
-var filter;
-var slice;
+var map = function() {
 
+};
 
-/* cursory type validation */
+var filter = function() {
 
-function isString(arg) {
-  return (typeof arg === 'string')
-}
+};
 
-function isArray(arg) {
-  return (arg instanceof Array)
-}
+var slice = function() {
+ 
+};
